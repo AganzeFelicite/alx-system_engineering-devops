2cad493d97d1253d9c47707dd430be52fe8fced6
AganzeFelicite <aganzefelicite@gmail.com>
2022-09-21 11:32:54 +0200

    configuration management

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# this a puppet to create a file
+  file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+  }
