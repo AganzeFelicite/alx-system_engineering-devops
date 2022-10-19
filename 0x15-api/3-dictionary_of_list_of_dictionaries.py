e500ade151f880be9ab07499a3646e598cfd5c64
AganzeFelicite <aganzefelicite@gmail.com>
2022-10-19 12:45:58 +0200

    export into json

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""this is to get employee is information"""
+
+
+import json
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    url1 = "https://jsonplaceholder.typicode.com/users/"
+    url2 = "https://jsonplaceholder.typicode.com/todos"
+    info = requests.get(url1).json()
+    todo = requests.get(url2).json()
+    data = []
+    my_dict = {}
+    for item in info:
+        data = []
+        for row in todo:
+            if item.get('id') == row.get('userId'):
+                a_row = ({
+                    "username": item.get('name'),
+                    "task": row.get('title'),
+                    "completed": row.get('completed')
+                    })
+                data.append(a_row)
+        my_dict[item.get('id')] = data
+    with open('todo_all_employees.json', 'w') as new_file:
+        json.dump(my_dict, new_file)
