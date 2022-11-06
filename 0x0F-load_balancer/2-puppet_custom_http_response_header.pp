f50f44234d5b8644ddb6325753cd4fe5e606cada
AganzeFelicite <aganzefelicite@gmail.com>
2022-11-06 16:02:44 +0200

    puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,12 +1,7 @@
-#to set up a new server with all the configuration we have 
-#done sofar using bash
-
+# A puppet manifest to configure ubuntu
+# installs a Nginx server
 
 exec {'install Nginx':
   provider => shell,
-  command  => 'sudo apt-get -y update,
-  sudo apt-get -y install nginx;echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html;
-  sudo sed -i "s/server_name _;/server_name _;\n\tadd_header X-Served-By $(hostname);
-  \n\trewrite ^\/redirect_me https:\/\/github.com\/Theocode12 permanent;/"/etc/nginx/sites-available/default;
-  sudo service nginx start'
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\tadd_header X-Served-By: $(hostname);\n\trewrite ^\/redirect_me https:\/\/github.com\/Theocode12 permanent;/" /etc/nginx/sites-available/default; sudo service nginx start',
 }
