34804331546971996ae4d17dd0197c8ef5f41cf6
AganzeFelicite <aganzefelicite@gmail.com>
2022-11-06 15:47:33 +0200

    puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -10,5 +10,6 @@ exec {'install Nginx':
   command  => sudo sed -i "s/server_name _;/server_name _,
   command  => \n\tadd_header X-Served-By: $(hostname);
   command  => \n\trewrite ^\/redirect_me https:\/\/github.com\/Theocode12 permanent;/" 
-  /etc/nginx/sites-available/default; sudo service nginx start',
+  command  => /etc/nginx/sites-available/default; sudo service nginx start',
+
 }
