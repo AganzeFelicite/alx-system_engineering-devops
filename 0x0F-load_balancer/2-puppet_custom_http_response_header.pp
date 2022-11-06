12f3b86c08011d02bcd1fdbab686cc7af333237d
AganzeFelicite <aganzefelicite@gmail.com>
2022-11-06 15:27:12 +0200

    learning puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,4 @@
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\tadd_header X-Served-By: $(hostname);\n\trewrite ^\/redirect_me https:\/\/github.com\/Theocode12 permanent;/" /etc/nginx/sites-available/default; sudo service nginx start',
+}
