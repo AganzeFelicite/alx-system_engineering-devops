a4ce0bd60701d51a8bfe04cf61ef237c2ed6a985
AganzeFelicite <aganzefelicite@gmail.com>
2022-11-06 15:54:31 +0200

    puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,11 +4,11 @@
 
 exec {'install Nginx':
   provider => shell,
-  command  => 'sudo apt-get -y update,
-  command  => sudo apt-get -y install nginx,
-  command  => echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html,
-  command  => sudo sed -i "s/server_name _;/server_name _;\n\tadd_header X-Served-By $(hostname);
- 	 \n\trewrite ^\/redirect_me https:\/\/github.com\/Theocode12 permanent;/"/etc/nginx/sites-available/default,
-  command  => sudo service nginx start',
+  command  => 'sudo apt-get -y update',
+  command  => 'sudo apt-get -y install nginx',
+  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html',
+  command  => 'sudo sed -i "s/server_name _;/server_name _;\n\tadd_header X-Served-By $(hostname);
+ 	 \n\trewrite ^\/redirect_me https:\/\/github.com\/Theocode12 permanent;/"/etc/nginx/sites-available/default',
+  command  => 'sudo service nginx start',
 
 }
