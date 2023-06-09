7619f56a2081a8ff5cf764048b5451bf1e7e4c27
AganzeFelicite <aganzefelicite@gmail.com>
2023-06-09 16:22:45 +0200

    () changed to []

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -3,11 +3,11 @@
 exec{'replacing':
   provider => shell,
   command  => "sudo sed -i 's/nofile 5/nofile 500000/' /etc/security/limits.conf",
-  before   => Exec('replacing-1st')
+  before   => Exec['replacing-1st'],
 
 }
 exec{'replacing-1st':
 provider => shell,
-command  => "sudo sed -i 's/nofile 4/nofile 40000/' /etc/security/limits.conf"
+command  => "sudo sed -i 's/nofile 4/nofile 40000/' /etc/security/limits.conf",
 }
 
