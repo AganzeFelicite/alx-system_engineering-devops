f9400b4f78d3dbb81979ff9490bbadc086abc037
AganzeFelicite <aganzefelicite@gmail.com>
2023-06-09 16:17:02 +0200

    user nofile increased

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# the problem of high number of files open fixed
+
+exec{'replacing':
+  provider => shell,
+  command  => "sudo sed -i 's/nofile 5/nofile 500000/' /etc/security/limits.conf",
+  before   => Exec('replacing-1st')
+
+}
+exec{'replacing-1st':
+provider => shell,
+command  => "sudo sed -i 's/nofile 4/nofile 500000/' /etc/security/limits.conf"
+}
+
