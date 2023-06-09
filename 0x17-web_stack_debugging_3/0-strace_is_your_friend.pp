9a51520a766524f0e3985d973e828f42724d3c47
AganzeFelicite <aganzefelicite@gmail.com>
2023-06-09 14:29:10 +0200

    path specified

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,7 @@
 # Fixes Apache 500 error
 
 exec { 'fix-the 500 error in apache/ wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/',
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php'
+
 }
