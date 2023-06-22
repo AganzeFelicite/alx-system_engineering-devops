825ede11f6acd6a2ccf8f60a22d52cf007989dfe
AganzeFelicite <aganzefelicite@gmail.com>
2023-06-22 11:28:08 +0200

    setting up the replica

--- 0x14-mysql/4-slave_queries.sql
+++ 0x14-mysql/4-slave_queries.sql
@@ -2,7 +2,7 @@
 -- Queries (Must change to correct data)
 -- Master values
 -- SHOW MASTER STATUS; (in master)
-CHANGE MASTER TO MASTER_HOST='34.73.70.32',MASTER_USER='replica_user',
+CHANGE MASTER TO MASTER_HOST='100.25.220.47',MASTER_USER='replica_user',
 MASTER_PASSWORD='replica_user_pwd', MASTER_LOG_FILE='mysql-bin.000001', MASTER_LOG_POS= 154;
 
 START SLAVE;
